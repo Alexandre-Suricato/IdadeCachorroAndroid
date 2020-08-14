842ebc3a0e5547335f4f1967620f757a99f8ab56
suricatoti <alexandredslima@gmail.com>
2020-08-14 11:40:22 -0300

    First Commit

--- app/src/main/java/br/com/zup/idadecachorroandroid/AppBeagleConfig.kt
+++ app/src/main/java/br/com/zup/idadecachorroandroid/AppBeagleConfig.kt
@@ -6,13 +6,9 @@ import br.com.zup.beagle.android.setup.Cache
 import br.com.zup.beagle.android.setup.Environment
 
 @BeagleComponent
-class AppBeagleConfig : BeagleConfig {
-    override val isLoggingEnabled: Boolean = true
-    override val baseUrl: String get() = "https://10.0.2.2:8080/"
-    override val environment: Environment get() = Environment.DEBUG
-    override val cache: Cache = Cache(
-        enabled = true, // Habilita ou desabilita o cache em memória e disco.
-        maxAge = 300, // Tempo em segundos que o cache em memória estará ativo.
-        memoryMaximumCapacity = 15 // Tamanho do cache de memória LRU. Representa o número de telas que vão estar na memória.
-    )
-}
+class AppBeagleConfig(
+    override val isLoggingEnabled: Boolean = true,
+    override val baseUrl: String = "http://10.0.2.2:8080",
+    override val cache: Cache = Cache(enabled = false, maxAge = 5L, memoryMaximumCapacity = 1),
+    override val environment: Environment = Environment.DEBUG
+) : BeagleConfig
